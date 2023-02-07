a698a5ed8943d73b9284cc37fb17cd5d8bcc14d2
Tommy Oladiti <tommy.oladiti@roq.local>
2023-02-07 15:58:44 +0000

    Intial git commit with fibonnaci

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -10,3 +10,36 @@ test('When initialized the next number can be returned', () => {
     fibonacci.init(8);
     expect(fibonacci.next()).toBe(13);
 });
+
+
+test('When initialized not specified the next number can be returned', () => {
+    fibonacci.init();
+    expect(fibonacci.next()).toBe(1);
+});
+
+
+test('When initialized the next number can be returned', () => {
+    fibonacci.init(2);
+    expect(fibonacci.next()).toBe(3);
+});
+
+test('When initialized the next number can be returned when skipped by 1', () => {
+    fibonacci.init(2);
+    expect(fibonacci.skip(1)).toBe(3);
+});
+
+
+test('When initialized skip through 2 numbers and the next number can be returned', () => {
+    fibonacci.init(8);
+    expect(fibonacci.skip(2)).toBe(21);
+});
+
+test('When initialized, check the current number', () => {
+    fibonacci.init(8);
+    expect(fibonacci.init()).toBe(8);
+});
+
+test('When initialized the next number can be returned', () => {
+    fibonacci.init(0);
+    expect(fibonacci.skip(3)).toBe(2);
+});
